f8b725151a6e89366d80ac9789f2ae644365a8b4
Aaron Gu <ag5ym@virginia.edu>
2017-12-02 13:25:24 -0500

    minor changes

--- src/UltimateTicTacToe.java
+++ src/UltimateTicTacToe.java
@@ -21,7 +21,7 @@ public class UltimateTicTacToe {
 		protected void paintComponent(Graphics g) {
 			Graphics2D g2 = (Graphics2D) g;
 			g2.setStroke(new BasicStroke(10));
-			g2.drawLine(170, 0, 170, 450);
+			g2.drawLine(150, 0, 150, 450);
 			g2.drawLine(300, 0, 300, 450);
 			g2.drawLine(0, 150, 450, 150);
 			g2.drawLine(0, 300, 450, 300);
@@ -33,7 +33,7 @@ public class UltimateTicTacToe {
 		createUltimateBoard();
 		frame.setVisible(true);
 		frame.setTitle("Ultimate Tic-Tac-Toe");
-		frame.setSize(700, 700);
+		frame.setSize(450, 475);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);	
 		frame.setLocationRelativeTo(null);
 	}
@@ -49,9 +49,9 @@ public class UltimateTicTacToe {
 	
 	public void createBasicBoard(){
 		JPanel panel = new JPanel() {
-			protected void paintComponent(Graphics g) {
+			protected void paintComponent(Graphics g) { //resets the stroke from the previous graphics2D
 				Graphics2D g2 = (Graphics2D) g;
-				g2.setStroke(new BasicStroke(3));
+				g2.setStroke(new BasicStroke(2));
 				g2.drawLine(50, 0, 50, 300);
 				g2.drawLine(100, 0, 100, 300);
 				g2.drawLine(0, 50, 300, 50);
@@ -63,7 +63,6 @@ public class UltimateTicTacToe {
 			for(int c = 0; c<3; c++) {
 				xo[r][c] = true;
 				JButton matrixAdd = new JButton();
-				matrixAdd.setPreferredSize(new Dimension(50,50));
 				matrixAdd.addActionListener(new MoveActionListener());
 				buttonMatrix[r][c] = matrixAdd;
 				panel.add(buttonMatrix[r][c]);
