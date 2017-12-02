06328719422cb4583034a64b3fe9c70916cc03df
Aaron Gu <ag5ym@virginia.edu>
2017-12-02 11:37:40 -0500

    added grid lines

--- src/BasicTicTacToe.java
+++ src/BasicTicTacToe.java
@@ -1,3 +1,5 @@
+import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,12 +16,20 @@ public class BasicTicTacToe extends JPanel{
 	public boolean turn = true; // true is X, false is O, X moves first
 	
 	public BasicTicTacToe(){
-		JPanel panel = new JPanel();
+		JPanel panel = new JPanel() {
+			protected void paintComponent(Graphics g) {
+				g.drawLine(50, 0, 50, 300);
+				g.drawLine(100, 0, 100, 300);
+				g.drawLine(0, 50, 300, 50);
+				g.drawLine(0, 100, 300, 100);
+			}
+		};
 		panel.setLayout(new GridLayout(3,3));
 		for(int r = 0; r<3; r++) {
 			for(int c = 0; c<3; c++) {
 				xo[r][c] = true;
 				JButton matrixAdd = new JButton();
+				matrixAdd.setPreferredSize(new Dimension(50,50));
 				matrixAdd.addActionListener(new MoveActionListener());
 				buttonMatrix[r][c] = matrixAdd;
 				panel.add(buttonMatrix[r][c]);
